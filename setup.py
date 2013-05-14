8a04f32e37899f4e162d6657ca7202882002399f
Benoît Suttor <bsuttor@cirb.irisnet.be>
2013-05-14 11:45:19 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+version = '1.0'
+
+setup(name='policy.contactcenter',
+      version=version,
+      description="",
+      long_description=open("README.rst").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Benoit Suttor',
+      author_email='bsuttor@cirb.irisnet.be',
+      url='https://github.com/collective/',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['policy'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+          'Products.LinguaPlone',
+          'cirb.zopemonitoring',
+          # -*- Extra requirements: -*-
+          'plonetheme.contactcenter',
+          'webcouturier.dropdownmenu',
+          'collective.quickupload',
+          'Products.PloneFormGen',
+          'Solgema.fullcalendar',
+          'collective.easyslider',
+          'collective.ckeditor',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
